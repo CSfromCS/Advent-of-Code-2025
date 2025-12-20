82ae324fdfa2e4e7e61bec1f5e3665511143b8f4
Christian Sarabia <csarabia@microsoft.com>
2025-12-20 23:18:01 +1100

    Add option to read logs

---
+++ Common/Logger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2025.Common
+{
+    public static class Logger
+    {
+        public static bool showLogs = false;
+        public static void Log(object? message = null)
+        {
+            if (showLogs)
+            {
+                Console.WriteLine(message);
+            }
+        }
+
+        public static void LogNoEnter(object? message = null)
+        {
+            if (showLogs)
+            {
+                Console.Write(message);
+            }
+        }
+
+        public static void Report(object? message = null)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AdventOfCode2025.Common;
 
 namespace AdventOfCode2025
 {
@@ -13,19 +14,24 @@ namespace AdventOfCode2025
             Console.WriteLine("Hello, Advent of Code 2025!");
 
             Console.WriteLine("Please enter the DAY you want to run (e.g., 1):");
-            string input = "3";// Console.ReadLine();
-            if (int.TryParse(input, out int dayNumber))
-            {
-                string dayClassName = $"AdventOfCode2025.Day{dayNumber:D2}";
-                Type? dayType = Type.GetType(dayClassName);
-                var mainMethod = dayType.GetMethod("Main", System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static);
-                mainMethod.Invoke(null, null);
-                // Not safe yet but works for simple starting UI
-            }
-            else
+            string input = "4";// Console.ReadLine();
+            if (!int.TryParse(input, out int dayNumber))
             {
                 Console.WriteLine("Invalid input. Please enter a valid DAY number.");
+                return;
             }
+
+            Console.WriteLine("Do you want to enable logging? (y/n):");
+            if (Console.ReadLine().Trim().ToLower() == "y")
+            {
+                Logger.showLogs = true;
+            }
+
+            string dayClassName = $"AdventOfCode2025.Day{dayNumber:D2}";
+            Type? dayType = Type.GetType(dayClassName);
+            var mainMethod = dayType.GetMethod("Main", System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static);
+            mainMethod.Invoke(null, null);
+            // Not safe yet but works for simple starting UI\
         }
     }
 }
